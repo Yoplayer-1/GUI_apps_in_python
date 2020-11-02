1204c5d48d08075e7e7c6abfa45c19d85371db9f
Yoplayer-1 <66262055+Yoplayer-1@users.noreply.github.com>
2020-11-02 10:29:25 +0530

    Add files via upload

---
+++ firstvid.py
@@ -0,0 +1,14 @@
+from tkinter import * 
+
+root = Tk()
+
+#Creating the lable widget
+my_label = Label(root, text="Hello World!")
+
+#Packing the lable on the screen
+my_label.pack()
+
+
+
+
+root.mainloop()
